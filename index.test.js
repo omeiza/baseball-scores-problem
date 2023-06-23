c6342015fdf2cff661b4be3e9a91d8e7829a9740
Omeiza Owuda <owudaomeiza@gmail.com>
2023-06-23 03:18:37 +0100

    Added test description

--- index.test.js
+++ index.test.js
@@ -1,29 +1,31 @@
 import getTotalScore from "./index";
 
-/**
- * Test Case 1
- * input = "5 2 C D +"
- * expected value = 30
- */
-test("Gets total score of baseball board: '5 2 C D +' to equal 30 😊", () => {
-	expect(getTotalScore("5 2 C D +")).toBe(30);
-});
+describe("Interprete baseball scoreboard", () => {
+	/**
+	 * Test Case 1
+	 * input = "5 2 C D +"
+	 * expected value = 30
+	 */
+	test("Gets total score of baseball board: '5 2 C D +' to equal 30 😊", () => {
+		expect(getTotalScore("5 2 C D +")).toBe(30);
+	});
 
-/**
- * Test Case 2
- * input = "5 -2 4 C D 9 + +"
- * expected value = 27
- */
-test("Gets total score of baseball board: '5 -2 4 C D 9 + +' to equal 27 😊", () => {
-	expect(getTotalScore("5 -2 4 C D 9 + +")).toBe(27);
-});
+	/**
+	 * Test Case 2
+	 * input = "5 -2 4 C D 9 + +"
+	 * expected value = 27
+	 */
+	test("Gets total score of baseball board: '5 -2 4 C D 9 + +' to equal 27 😊", () => {
+		expect(getTotalScore("5 -2 4 C D 9 + +")).toBe(27);
+	});
 
-/**
- * Test Case 3
- * input = "1"
- * expected value = 1
- */
-test("Gets total score of baseball board: '1' to equal 1 😊", () => {
-	expect(getTotalScore("1")).toBe(1);
+	/**
+	 * Test Case 3
+	 * input = "1"
+	 * expected value = 1
+	 */
+	test("Gets total score of baseball board: '1' to equal 1 😊", () => {
+		expect(getTotalScore("1")).toBe(1);
+	});
 });
 
